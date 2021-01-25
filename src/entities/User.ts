990e3576fab0d2aad46e82ac2118473eb7dc4e4d
Aluru Murali Krishna <alurumuralikrishna9@gmail.com>
2021-01-26 01:45:25 +0530

    Project set up and Added User Entity

---
+++ src/entities/User.ts
@@ -0,0 +1,55 @@
+import { IsEmail, Length } from 'class-validator';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  BaseEntity,
+  Index,
+  CreateDateColumn,
+  UpdateDateColumn,
+  BeforeInsert,
+} from 'typeorm';
+import bcrypt from 'bcrypt';
+import { classToPlain, Exclude } from 'class-transformer';
+
+@Entity('users')
+export class User extends BaseEntity {
+  constructor(user: Partial<User>) {
+    super();
+    Object.assign(this, user);
+  }
+
+  @Exclude()
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Index()
+  @IsEmail()
+  @Column({ unique: true })
+  email: string;
+
+  @Exclude()
+  @Length(6, 255)
+  @Column()
+  password: string;
+
+  @Index()
+  @Length(3, 255, { message: 'Username must be atleast 3 characters long' })
+  @Column({ unique: true })
+  username: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @BeforeInsert()
+  async hashPassword() {
+    this.password = await bcrypt.hash(this.password, 6);
+  }
+
+  toJSON() {
+    return classToPlain(this);
+  }
+}
---
+++ src/routes/auth.ts
@@ -0,0 +1,44 @@
+import { validate } from 'class-validator';
+
+import { Request, Response, Router } from 'express';
+
+import { User } from './../entities/User';
+
+const register = async (req: Request, res: Response) => {
+  const { email, username, password } = req.body;
+
+  try {
+    // TODO: Validate data
+    let errors: any = {};
+    const emailUser = await User.findOne({ email });
+    const usernameUser = await User.findOne({ username });
+
+    if (emailUser) errors.email = 'Email is already taken';
+    if (usernameUser) errors.username = 'Username is already taken';
+
+    if (Object.keys(errors).length > 0) {
+      return res.status(400).json(errors);
+    }
+
+    // TODO: Create the user
+    const user = new User({ email, password, username });
+
+    // Validate the user object
+    errors = await validate(user);
+    if (errors.length > 0) return res.status(400).json({ errors });
+    await user.save();
+
+    // TODO: Return the user
+    return res.json(user);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      error: err,
+    });
+  }
+};
+
+const router = Router();
+router.post('/register', register);
+
+export default router;
